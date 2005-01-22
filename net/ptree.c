b0f6c71dcfb8cc306aff22b0df7e01aa5e81a210
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-01-23 03:40:11 +0900

    updata 2010/10/27 16:01.

--- net/ptree.c
+++ net/ptree.c
@@ -17,7 +17,7 @@
 #endif /*_KERNEL*/
 
 #define DEBUG 0
-#define dprint(x) { if(DEBUG) printf x; else return 0; }
+#define dprint(x) { if(DEBUG) printf x; else return; }
 
 char mask[] = { 0x00, 0x80, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc, 0xfe, 0xff };
 
