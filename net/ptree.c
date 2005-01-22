5ddbde4972f3401f52f69a69a88cfcafd74d0f87
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-01-22 18:17:55 +0900

    updata 2010/10/27 06:39.

--- net/ptree.c
+++ net/ptree.c
@@ -150,8 +150,7 @@ ptree_search (char *key, int keylen, struct ptree *t)
 	dprint(("ptree_search: search_key = %p len = %d\n",key,keylen));
 	struct ptree_node *x, *match;
 
-	match = NULL;
-	x = t->top;
+	match = x = t->top;
 	dprint(("ptree_search: check node keylen flag\n"));
 	while (x && x->keylen <= keylen &&
 			ptree_match (x->key, key, x->keylen))
