9eb3ff2916a050206b3f0e58d0cdb60a0f6b6211
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-01-23 06:59:45 +0900

    updata 2010/10/27 19:21.

--- net/ptree.c
+++ net/ptree.c
@@ -97,8 +97,8 @@ ptree_match (char *keyi, char *keyj, int keylen)
 	dprint(("  ptree_match: keylen = %d bytes = %d bits = %d\n",keylen,bytes,bits));
 
 	dprint(("  ptree_match: !memcmp(keyi,keyj) = %d",!memcmp(keyi,keyj,bytes)));
-	dprint((" keyi = %x keyj = %x mask = %x\n",keyi[bytes],keyj[bytes],mask[bits]));
-	dprint((" !(keyi^keyj) & mask = %d\n",!(keyi[bytes]^keyj[bytes])&mask[bits]));
+	//dprint((" keyi = %x keyj = %x mask = %x\n",keyi[bytes],keyj[bytes],mask[bits]));
+	dprint((" !(keyi^keyj) & mask = %d\n",!((keyi[bytes]^keyj[bytes])&mask[bits])));
 	if (! memcmp (keyi, keyj, bytes) &&
 		       (!(keyi[bytes] ^ keyj[bytes]) & mask[bits])){
 		dprint(("  ptree_match End (%d bit match)\n",keylen));
