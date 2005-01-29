79f4c8be4000f9f5cc5ba0b8ebbf4d773b9b1e97
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-01-30 05:29:09 +0900

    update 2010/11/03 17:50.

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -948,8 +948,8 @@ ptree_walktree(h, f, w)
 				rt = (struct rtentry *)rn;
 				rt_gate = (struct sockaddr_in *)rt->rt_gateway;
 				rn_dst = (struct sockaddr_in *)rt_key(rt);
-				*gate = rt_gate->sin_addr;
-				*dst = rn_dst->sin_addr;
+				gate = &rt_gate->sin_addr;
+				dst = &rn_dst->sin_addr;
 				ip = (unsigned char *)gate;
 				printf("address: %x.%x.%x.%x\n",*ip,*ip+1,*ip+2,*ip+3);
 				printf("address: %d.%d.%d.%d\n",*ip,*ip+1,*ip+2,*ip+3);
