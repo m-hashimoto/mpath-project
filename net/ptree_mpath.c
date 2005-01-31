b42bd61dd1b548b465c7259b7a828d8c9d148ae4
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-02-01 01:44:04 +0900

    update 2010/11/05 14:05.

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -141,7 +141,6 @@ static int ptree_satisfies_leaf(char *trial,
 	
 	if(!t){
 		dprint(("-ptree_insert: search(v) = NULL\n"));
-		b = -1 - vlen;
 		goto on2;
 	}
 	/* Find first bit at which v and t->rn_key differ */ 
