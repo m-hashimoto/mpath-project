687dfebbb86c95a76c677b83d7dae8ef46fc3f47
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-02-11 10:28:00 +0900

    update

--- net/ptree_mpath.h
+++ net/ptree_mpath.h
@@ -92,10 +92,9 @@ struct ptree_node
 					   	struct ptree_node *),
 		*ptree_deladdr(void *, void *, struct ptree_node_head *),
 		*ptree_matchaddr(void *, struct ptree_node_head *);
-#ifdef DEBUG
+
 int debug_node_print(struct ptree_node *);
 int debug_tree_print(struct ptree_node_head *);
-#endif
 
 #ifdef PTREE_MPATH
 #ifdef _KERNEL
