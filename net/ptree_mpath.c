f830aea940a0c70fcc1ef7ce413665109692f293
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-02-11 09:03:10 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -587,8 +587,8 @@ ptree_addroute(v_arg, n_arg, head, rt_node)
 		saved_tt = tt = ptree_insert(v_arg, n_arg, head, &keyduplicated);
 		tt->data = rt;
 		rt_node = tt;
-		dprint(("-ptree_addroute: tt = %p rt_node = %p keydup = %d\n",
-								tt,rt_node,keyduplicated));
+		dprint(("-ptree_addroute: tt = %p rt = %p keydup = %d\n",
+								tt,rt,keyduplicated));
 #if 0 /* multi path */
 		if (keyduplicated) {
 				for (t = tt; tt; t = tt, tt = tt->rn_dupedkey) {
