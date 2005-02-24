a04f92c832aede5b1dc377019467ee786669cd15
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-02-24 12:49:14 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -31,25 +31,21 @@ debug_node_print(struct ptree_node *pn, int offset)
 		struct rtentry *rt = pn->data;
 		unsigned char *gateway = (unsigned char *)rt->rt_gateway;
 
-		printf("[%d.%d.%d.%d|%d.%d.%d.%d|%d.%d.%d.%d|%d.%d.%d.%d/%d] ",
+		printf("[%d.%d.%d.%d|%d.%d.%d.%d|%d.%d.%d.%d/%d] ",
 						(unsigned char)pn->key[0],(unsigned char)pn->key[1],
 						(unsigned char)pn->key[2],(unsigned char)pn->key[3],
 						(unsigned char)pn->key[4],(unsigned char)pn->key[5],
 						(unsigned char)pn->key[6],(unsigned char)pn->key[7],
 						(unsigned char)pn->key[8],(unsigned char)pn->key[9],
 						(unsigned char)pn->key[10],(unsigned char)pn->key[11],
-						(unsigned char)pn->key[12],(unsigned char)pn->key[13],
-						(unsigned char)pn->key[14],(unsigned char)pn->key[15],
 						pn->keylen - 8*offset);
-		printf("[%d.%d.%d.%d|%d.%d.%d.%d|%d.%d.%d.%d|%d.%d.%d.%d] ",
+		printf("[%d.%d.%d.%d|%d.%d.%d.%d|%d.%d.%d.%d] ",
 						(unsigned char)gateway[0],(unsigned char)gateway[1],
 						(unsigned char)gateway[2],(unsigned char)gateway[3],
 						(unsigned char)gateway[4],(unsigned char)gateway[5],
 						(unsigned char)gateway[6],(unsigned char)gateway[7],
 						(unsigned char)gateway[8],(unsigned char)gateway[9],
-						(unsigned char)gateway[10],(unsigned char)gateway[11],
-						(unsigned char)gateway[12],(unsigned char)gateway[13],
-						(unsigned char)gateway[14],(unsigned char)gateway[15]);
+						(unsigned char)gateway[10],(unsigned char)gateway[11])
 		printf("[0x%x]\n",rt->rt_flags);
 	}
 #if 0
