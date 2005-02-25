15b000fa5672b862ae4026f7fa02ac26fca7ba95
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-02-26 04:10:47 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -27,7 +27,7 @@ debug_node_print(struct ptree_node *pn, int offset)
 	unsigned char *gateway = (unsigned char *)rt->rt_gateway;
 	
 	if(offset == 8){ /* IPv6 */
-		printf("[%X.%X.%X.%X.%X.%X.%X.%X.%X.%X.%X.%X/%d] ",
+		printf("[%2X.%2X.%2X.%2X.%2X.%2X.%2X.%2X.%2X.%2X.%2X.%2X/%d] ",
 				(unsigned char)pn->key[8],(unsigned char)pn->key[9],
 				(unsigned char)pn->key[10],(unsigned char)pn->key[11],
 				(unsigned char)pn->key[12],(unsigned char)pn->key[13],
@@ -35,7 +35,7 @@ debug_node_print(struct ptree_node *pn, int offset)
 				(unsigned char)pn->key[16],(unsigned char)pn->key[17],
 				(unsigned char)pn->key[18],(unsigned char)pn->key[19],
 				pn->keylen - 8*offset);
-		printf("[%X.%X.%X.%X.%X.%X.%X.%X] ",
+		printf("[%2X.%2X.%2X.%2X.%2X.%2X.%2X.%2X] ",
 				gateway[8],gateway[9],gateway[10],gateway[11],
 				gateway[12],gateway[13],gateway[14],gateway[15]);
 		
