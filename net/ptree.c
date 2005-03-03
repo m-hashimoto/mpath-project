7c2d6da8b66c438410e219c8e9995a54651b4ec8
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-03-04 05:22:42 +0900

    update

--- net/ptree.c
+++ net/ptree.c
@@ -38,6 +38,7 @@ ptree_node_create (char *key, int keylen)
   x->child[0] = NULL;
   x->child[1] = NULL;
   x->data = NULL;
+  x->lock = 1;
 
   /* fill in the key */
   memcpy (x->key, key, keylen);
