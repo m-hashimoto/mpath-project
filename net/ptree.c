b5ba62cf191a81de31db6e76f574fae4a3c6babe
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-03-05 00:54:00 +0900

    update

--- net/ptree.c
+++ net/ptree.c
@@ -101,6 +101,9 @@ ptree_lookup (char *key, int keylen, struct ptree *t)
   struct ptree_node *x;
 
   x = t->top;
+  if (!x)
+    return NULL;
+  
   while (x && x->keylen <= keylen &&
          ptree_match (x->key, key, x->keylen))
     x = x->child[check_bit (key, x->keylen)];
