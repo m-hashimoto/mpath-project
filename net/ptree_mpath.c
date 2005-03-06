e88496192944650c0f21dd09fdf0c3240cac16a2
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-03-07 06:38:51 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -29,11 +29,11 @@ sprint_inet_ntoa(int af, void *sa)
 	if(af == AF_INET){
 		char str[INET_ADDRSTRLEN];
 		__rpc_inet_ntop(af, &((struct sockaddr_in *)sa)->sin_addr, str, INET_ADDRSTRLEN);
-		return (str);
+		return (&str);
 	} else if(af == AF_INET6) {
 		char str[INET6_ADDRSTRLEN];
 		__rpc_inet_ntop(af, &((struct sockaddr_in6 *)sa)->sin6_addr, str, INET6_ADDRSTRLEN);
-		return (str);
+		return (&str);
 	}
 	return (NULL);
 }
