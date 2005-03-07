834191e2d3137a86b9aab5b6cf423a515ef8ae8e
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-03-08 05:30:30 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -287,17 +287,20 @@ ptree_addroute(v_arg, n_arg, head, rt_node)
 		saved_tt = tt = ptree_insert(v_arg, n_arg, head, &keyduplicated);
 #ifdef PTREE_MPATH /* multi path */
 		if (keyduplicated) {
-				if (tt->data) {
-						int n;
-						struct rtentry *rt0, *rt, **rt_array;
-						
-						rt = (struct rtentry *)rt_node;
-						rt0 = tt->data;
-						dprint(("-ptree_addroute: rt0[%p] rt[%p]\n",rt0,rt));
-						n = ptree_mpath_count(rt0);
-						rt_array = rt0->mpath_array;
-						rt_array[n] = rt;
-				}
+			int n;
+			struct rtentry *rt0, *rt, **rt_array;
+				
+			rt = (struct rtentry *)rt_node;
+			rt0 = tt->data;
+			dprint(("-ptree_addroute: rt0[%p] rt[%p]\n",rt0,rt));
+			n = ptree_mpath_count(rt0);
+			rt_array = rt0->mpath_array;
+			rt_array[n] = rt;
+		} else {
+			struct rtentry *rt = tt->data;
+			rt->mpath_array = NULL;
+			dprint(("-ptree_addroute: rt->mpath_array=%p\n",rt->mpath_array));
+			dprint(("-ptree_addroute: rt.mpath_array[0]=%p\n",rt->mpath_array[0]));
 		}
 #endif /* mluti path */
 		return tt;
