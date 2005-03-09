04eff944690ca0603795865a1493cc8536bfb3c1
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-03-10 03:19:21 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -298,7 +298,8 @@ ptree_addroute(v_arg, n_arg, head, rt_node)
 			rt_array[n] = rt;
 		} else {
 			struct rtentry *rt = tt->data;
-			struct rtentry **rt_array = rt.mpath_array;
+			struct rtentry **rt_array = rt->mpath_array;
+			rt = rt_array[0]; 
 			dprint(("-ptree_addroute: rt=%p\n",rt));
 			dprint(("-ptree_addroute: rt_array=%p\n",rt_array));
 			dprint(("-ptree_addroute: &rt_array=%p\n",&rt_array));
