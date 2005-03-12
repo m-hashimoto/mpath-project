b7b002d88c0aa2756c8668cedfb6b579d4e9c296
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2005-03-12 09:38:27 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -39,8 +39,8 @@ sprint_inet_ntoa(int af, void *sa)
 	int
 debug_node_print(struct ptree_node *pn, int offset)
 {
-		dprint(("-debug_node_print: pn[%p] key[%p] keylen[%d]\n",pn,pn->key,pn->keylen));
 	if(!pn->key){
+		dprint(("-debug_node_print: pn[%p] key[%p] keylen[%d]\n",pn,pn->key,pn->keylen));
 		dprint(("-debug_node_print: pn->parent[%p] pn->left[%p] pn->rigth[%p]\n",
 														pn->parent,pn->child[0],pn->child[1]));
 		return 1;
@@ -134,7 +134,7 @@ static int ptree_walktree(struct ptree_node_head *h, walktree_f_t *f, void *w);
 	char *v = v_arg, *m = m_arg;
 	register char *cp;
 	struct ptree_node *top = head->pnh_top, *t, *tt;
-	int len = 8*LEN(v);
+	int len = 8*(int)LEN(v);
 	
 #ifdef DEBUG
 	if(head->pnh_offset == 4){
