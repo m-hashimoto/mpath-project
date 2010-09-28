5993fe9d37b08d51e5d98f624e177841d7c6ba60
masahiko hashimoto <m-hashimoto@freebsd-vm1.jpcert.cc>
2010-09-28 22:34:30 +0900

    updata 2010/09/30 05:00.

--- install.sh
+++ install.sh
@@ -66,10 +66,11 @@ cd /usr/src/sys/fs/nfs/
 patch -N < nfsport.h.diff
 
 cd /usr/src/sys/i386/conf/
-config PATRICIA
+#config PATRICIA
+config -g PATRICIA
 
 cd ../compile/PATRICIA
 make cleandepend
 make depend
-make
+#make
 #make install
