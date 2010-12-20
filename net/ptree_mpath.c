086511da59214e20ad7ace7a3b4f7d770a4e46e2
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2010-12-20 22:42:08 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -362,7 +362,7 @@ ptree_addroute(v_arg, n_arg, head, rt_node)
 			if(n == MAX_MULTIPATH){
 				struct rtentry **tmp
 				if(R_Realloc(tmp, struct rtentry **, 
-												10*MAX_MULTIPATH*sizeof(struct rtentry *))){
+												10*MAX_MULTIPATH*sizeof(struct rtentry *))==NULL){
 					printf("realloc fault\n");
 					return EEXIST;
 				}
