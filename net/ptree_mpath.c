21fbd70802395c3d4b8c8d900d0aadcfc3bdb480
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2010-12-21 00:03:12 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -154,7 +154,7 @@ static int ptree_walktree(struct ptree_node_head *h, walktree_f_t *f, void *w);
 	//caddr_t v = v_arg, m = m_arg;
 	//register caddr_t cp;
 	char *v = v_arg, *m = m_arg;
-	//register char *cp;
+	register char *cp;
 	struct ptree_node *top = head->pnh_top, *t, *tt;
 	int len;
 	
@@ -183,10 +183,10 @@ static int ptree_walktree(struct ptree_node_head *h, walktree_f_t *f, void *w);
 		len = 8*len;
 
 		/* default gateway "0.0.0.0/0" */
-		char zero[len/8], *cp;
+		char zero[len/8], *tmp;
 		memset(zero,0,sizeof(zero));
-		cp = v + head->pnh_offset;
-		if(memcmp(cp,zero,sizeof(cp)) == NULL)
+		tmp = v+head->pnh_offset;
+		if(memcmp(tmp,zero,sizeof(tmp)) == 0)
 			len = 0;
 #ifdef DEBUG
 		if(head->pnh_offset == INET_HEADOFF){
