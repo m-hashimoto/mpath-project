9c219afdaa65a17d8ac24dad860019aca338332b
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2010-12-21 02:45:36 +0900

    update

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -218,7 +218,7 @@ static int ptree_walktree(struct ptree_node_head *h, walktree_f_t *f, void *w);
 on1:
 	*dupentry = 0;
 	/* default gateway "0.0.0.0/0" */
-	if(memcmp(v+head->pnh_offset,pn_zeros,sizeof(tmp)) == 0){
+	if(memcmp(v+head->pnh_offset,pn_zeros,len/8-head->pnh_offset) == 0){
 		tt->keylen = 8*head->pnh_offset;
 		dprint(("-ptree_insert: default gateway len[%d]\n",tt->keylen));
 	}
