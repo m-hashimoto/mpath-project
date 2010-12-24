cdb1eb764db65b229db8b64ea73c17b10d0b258d
User <m-hashimoto@m-hashimoto.jaist.ac.jp>
2010-12-25 00:14:31 +0900

    update RDTSC

--- net/ptree_mpath.c
+++ net/ptree_mpath.c
@@ -155,7 +155,7 @@ debug_tree_print(struct ptree_node_head *pnh)
 	register char *cp;
 	struct ptree_node *top = head->pnh_top, *t, *tt;
 	int len;
-	unsigned long c0,c1,c2;
+	long long int c0,c1,c2;
 	double td;
 	
 	if(head->pnh_offset == INET_HEADOFF )
@@ -192,11 +192,11 @@ debug_tree_print(struct ptree_node_head *pnh)
 	RDTSC(c1);
 	t = ptree_search(v, len, head->pnh_treetop);
 	RDTSC(c2);
-	printf("-ptree_insert: RDTSC  %lu clk\n",c0);
-	printf("-ptree_insert: search Start %lu clk\n",c1);
-	printf("-ptree_insert: search End   %lu clk\n",c2);
+	printf("-ptree_insert: RDTSC  %lld clk\n",c0);
+	printf("-ptree_insert: search Start %lld clk\n",c1);
+	printf("-ptree_insert: search End   %lld clk\n",c2);
 	td = (c2 - c1)/cpu_frequency;
-	printf("-ptree_insert: search interval %lu clk %12.10f s\n",c2-c1, td);
+	printf("-ptree_insert: search interval %lld clk %f s\n",c2-c1, td);
 	if (!t)
 		goto on1;
 
@@ -217,11 +217,11 @@ on1:
 	RDTSC(c1);
 	tt = ptree_add(v, len, data, head->pnh_treetop);
 	RDTSC(c2);
-	printf("-ptree_insert: RDTSC  %lu clk\n",c0);
-	printf("-ptree_insert: add Start %lu clk\n",c1);
-	printf("-ptree_insert: add End   %lu clk\n",c2);
+	printf("-ptree_insert: RDTSC  %lld clk\n",c0);
+	printf("-ptree_insert: add Start %lld clk\n",c1);
+	printf("-ptree_insert: add End   %lld clk\n",c2);
 	td = (c2 - c1)/cpu_frequency;
-	printf("-ptree_insert: add interval %lu clk %12.10f s\n",c2-c1,td);
+	printf("-ptree_insert: add interval %lld clk %f s\n",c2-c1,td);
 	return (tt);
 }
 
@@ -480,8 +480,8 @@ ptree_init()
 				*cp++ = -1;
 
 #ifdef DEBUG
-		cpu_frequency = 1999.78;
-		printf("RDTSC: CPU %6.2f MHz\n",cpu_frequency);
+		cpu_frequency = 1999780000;
+		printf("RDTSC: CPU %f MHz\n",cpu_frequency/1000000);
 #endif
 }
 
